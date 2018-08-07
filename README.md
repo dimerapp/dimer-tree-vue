a895127b1d77888ccfadcb0acc19a6174a6b9d6d
thetutlage <virk.officials@gmail.com>
2018-08-07 16:54:27 +0530

    feat: initial working commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# dimer-tree-vue
+
+[![travis-image]][travis-url]
+[![npm-image]][npm-url]
+
+## Change log
+
+The change log can be found in the [CHANGELOG.md](https://github.com/dimerapp/dimer-tree-vue/CHANGELOG.md) file.
+
+## Contributing
+
+Everyone is welcome to contribute. Please take a moment to review the [contributing guidelines](CONTRIBUTING.md).
+
+## Authors & License
+[thetutlage](https://github.com/thetutlage) and [contributors](https://github.com/dimerapp/dimer-tree-vue/graphs/contributors).
+
+MIT License, see the included [MIT](LICENSE.md) file.
+
+[travis-image]: https://img.shields.io/travis/dimerapp/dimer-tree-vue/master.svg?style=flat-square&logo=travis
+[travis-url]: https://travis-ci.org/dimerapp/dimer-tree-vue "travis"
+
+[npm-image]: https://img.shields.io/npm/v/dimer-tree-vue.svg?style=flat-square&logo=npm
+[npm-url]: https://npmjs.org/package/dimer-tree-vue "npm"
---
+++ test/tree.spec.js
@@ -0,0 +1,94 @@
+/*
+* tree-react
+*
+* (c) Harminder Virk <virk@adonisjs.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+const test = require('japa')
+const tree = require('..')
+const utils = require('@vue/server-test-utils')
+const Markdown = require('@dimerapp/markdown')
+const dedent = require('dedent')
+const Vue = require('vue')
+
+function freshInstance (options) {
+  const Component = Vue.extend()
+  Component.use(tree, options)
+  return Component.component('DimerTree')
+}
+
+test.group('Tree', (group) => {
+  test('process dimer content node to html', async (assert) => {
+    const template = 'Hello world'
+    const json = await (new Markdown(template)).toJSON()
+
+    const output = utils.render(freshInstance(), {
+      propsData: {
+        node: json.contents
+      }
+    })
+
+    assert.equal(output.text(), 'Hello world')
+  })
+
+  test('process dimer content node with ul', async (assert) => {
+    const template = dedent`
+    - This is li
+    `
+
+    const json = await (new Markdown(template)).toJSON()
+
+    const output = utils.render(freshInstance(), {
+      propsData: {
+        node: json.contents
+      }
+    })
+
+    assert.equal(output.find('ul li').length, 1)
+    assert.equal(output.find('ul li').text(), 'This is li')
+  })
+
+  test('process dimer content node with custom output', async (assert) => {
+    const template = dedent`
+    - This is li
+    `
+
+    const json = await (new Markdown(template)).toJSON()
+
+    const output = utils.render(freshInstance({
+      processFn: function (node, render, createElement) {
+        if (node.tag === 'li') {
+          return createElement('li', Object.assign(node.props, { class: 'foo' }), node.children.map(render))
+        }
+      }
+    }), {
+      propsData: {
+        node: json.contents
+      }
+    })
+
+    assert.equal(output.find('ul li.foo').length, 1)
+    assert.equal(output.find('ul li.foo').text(), 'This is li')
+  })
+
+  test('assing id to element', async (assert) => {
+    const template = dedent`
+    # Hello
+    `
+
+    const json = await (new Markdown(template, {
+      skipToc: true
+    })).toJSON()
+
+    const output = utils.render(freshInstance(), {
+      propsData: {
+        node: json.contents
+      }
+    })
+
+    assert.equal(output.find('#hello').text(), 'Hello')
+  })
+})
